0172e26c059b06ad2a39b0a0df9548404e639505
JPogodzinski <pogodzinskij@gmail.com>
2020-04-22 13:55:24 +0200

    Initial commit

---
+++ src/main/kotlin/com/nbapp/adminserver/AdminserverApplication.kt
@@ -0,0 +1,11 @@
+package com.nbapp.adminserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminserverApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdminserverApplication>(*args)
+}
