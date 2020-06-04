f676a9ef171d3c113ce4996cdcd678e5c2ce2b3c
lobby150 <rejwen10@interia.pl>
2020-06-04 18:49:40 +0200

    added deleting assignations by team id

--- src/main/kotlin/com/nbapp/adminserver/modules/assignation/AssignationService.kt
+++ src/main/kotlin/com/nbapp/adminserver/modules/assignation/AssignationService.kt
@@ -2,6 +2,7 @@ package com.nbapp.adminserver.modules.assignation
 
 import com.nbapp.adminserver.modules.player.Player
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AssignationService(private val assignationRepository: AssignationRepository) {
@@ -26,5 +27,11 @@ class AssignationService(private val assignationRepository: AssignationRepositor
 
     fun addAssignation(assignation: Assignation):Assignation {
         return assignationRepository.save(assignation)
+    }
+    @Transactional
+    fun deleteAllTeamAssignation(teamId : Int)
+    {
+
+        return assignationRepository.deleteAssignationByTeamId(teamId)
     }
 }
